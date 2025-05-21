ba7fb4358aa99f99f653134acb803bdc011e3869
Pepo <pepo.ospina@gmail.com>
2025-05-21 12:28:46 +0200

    get reminders works too

--- stack/functions/tests/unit/test-handler.test.ts
+++ stack/functions/tests/unit/test-handler.test.ts
@@ -14,12 +14,23 @@ describe('Create reminder', function () {
     await resetDB();
   });
 
-  it('verifies create', async () => {
-    const result = await services.reminders.repo.createReminder({
-      content: 'test reminder',
+  it('creates two reminders', async () => {
+    const result1 = await services.reminders.repo.createReminder({
+      content: 'test reminder 1',
       date: 1747820397000,
     });
 
-    expect(result).toBeDefined();
+    const result2 = await services.reminders.repo.createReminder({
+      content: 'test reminder 2',
+      date: 1747820497000,
+    });
+
+    expect(result1.id).toBeDefined();
+    expect(result2.id).toBeDefined();
+  });
+
+  it('gets all reminders', async () => {
+    const reminders = await services.reminders.repo.getReminders();
+    expect(reminders.length).toBe(2);
   });
 });
